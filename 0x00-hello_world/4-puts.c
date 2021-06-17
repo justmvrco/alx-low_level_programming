5f0338594b42ba5ab4c5f017eacaee705847bf17
justmvrco <moreirapiliki@gmail.com>
2021-06-17 13:00:59 -0700

    nadre2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
-#!/bin/bash
-
 #include <stdio.h>
 int main()
 {
-  char words = "Programming is like building a multilingual puzzle\";
+  char words[n]; = "Programming is like building a multilingual puzzle";
   puts(words);
   return 0;
 }
