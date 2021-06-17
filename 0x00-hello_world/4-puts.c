6bb7c4ce83822598a5f95fa40b0bacf40f3d77fe
justmvrco <moreirapiliki@gmail.com>
2021-06-17 14:03:00 -0700

    gtf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 /**
  * main - Prints string
- * Decricription: Prints
-"\"Programming is like building a multilingual puzzle"
+ * Decricription: Prints "\"Programming is like building a multilingual puzzle"
  * Return: 0
  */
+int main (void)
+{
+       puts("\"Programming is like building a multilingual puzzle");
+       return (0);
+}       
