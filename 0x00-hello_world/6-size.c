3ae7ee6981203e0dc1a70942193c8e6a3258c7da
justmvrco <moreirapiliki@gmail.com>
2021-06-18 07:04:42 -0700

    dd

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,20 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Prints string
+ * Decricription: Prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+int main(void)
+{
+char ctype;
+int andre;
+long int moreira;
+long long int marco;
+float andrea;
+printf("Size of a char: %lu byte(s)\n", sizeof(char_type));
+printf("Size of a int: %lu byte(s)\n", sizeof(int_type));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long_int_type));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long_long_int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float_type));
+return (0);
+}
