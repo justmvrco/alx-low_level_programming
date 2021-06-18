7c6b4c682071602a7f3cff2c831e45ceb707ac43
justmvrco <moreirapiliki@gmail.com>
2021-06-18 07:23:50 -0700

    dddd

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int andre;
 long int moreira;
 long long int marco;
 float andrea;
-printf("Size of a char: %lu byte(s)\n", sizeof(char_type));
-printf("Size of a int: %lu byte(s)\n", sizeof(int_type));
-printf("Size of a long int: %lu byte(s)\n", sizeof(long_int_type));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(long_long_int));
-printf("Size of a float: %lu byte(s)\n", sizeof(float_type));
+printf("Size of a char: %lu byte(s)\n", sizeof(ctype));
+printf("Size of a int: %lu byte(s)\n", sizeof(andre));
+printf("Size of a long int: %lu byte(s)\n", sizeof(moreira));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(marco));
+printf("Size of a float: %lu byte(s)\n", sizeof(andrea));
 return (0);
 }
