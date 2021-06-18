9374bc90f1852a693359ef376938d36885b2d79e
justmvrco <moreirapiliki@gmail.com>
2021-06-18 06:06:53 -0700

    andre

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Prints string
+ * Decricription: Prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a pierce of art,\n");
+return (0);
+}
