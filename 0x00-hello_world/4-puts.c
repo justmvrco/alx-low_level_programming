2aae3fdede9cccb3d54c8a51db07e46f72e32375
justmvrco <moreirapiliki@gmail.com>
2021-06-18 05:43:53 -0700

    ä

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  * Decricription: Prints "\"Programming is like building a multilingual puzzle"
  * Return: 0
  */
-int main (void)
+int main(void)
 {
-       puts("\"Programming is like building a multilingual puzzle");
-       return (0);
-}       
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
