836fb4887d25ad6418eb25c98dfb6b8c82a2cbb2
justmvrco <moreirapiliki@gmail.com>
2021-06-23 09:56:55 -0700

    marco fix

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,20 +2,36 @@
 #include "holberton.h"
 
 /**
- * main - prints the largest of 3 integers
- * Return: 0
+ * print_remaining_days - takes a date and prints how many days are
+ * left in the year, taking leap years into account
+ * @month: month in number format
+ * @day: day of month
+ * @year: year
+ * Return: void
  */
 
-int main(void)
+void print_remaining_days(int month, int day, int year)
 {
-	int a, b, c;
-	int largest;
+	if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
+	{
+		if (month >= 2 && day >= 60)
+		{
+			day++;
+		}
 
-	a = 972;
-	b = -98;
-	c = 0;
-
-	largest = largest_number(a, b, c);
-	printf("%d is the largest number\n", largest);
-	return (0);
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
+	}
+	else
+	{
+		if (month == 2 && day == 60)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else
+		{
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
+		}
+	}
 }
