1a1a216ba43dc57fd64a98f019694c8a38a557f7
justmvrco <moreirapiliki@gmail.com>
2021-06-24 07:54:48 -0700

    marco

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,38 +1,51 @@
 #include "holberton.h"
+
 /**
- * print_number - prints a number
- * @n: Input number
+ * print_number - print number
+ * @n: input value
+ *
+ * Description:  prints an integer
+ * Return: Always (0)
  */
 
 void print_number(int n)
 {
-	long len, res, i, temp, expo;
-
-	res = n;
-	expo = len =  1;
-/*Check negatives*/
-	if (res < 0)
-	{
-		res *= -1;
-		_putchar('-');
-	}
-
-/**/
-	temp = res;
-	while (temp >= 10)
-	{
-		len++;
-		temp /= 10;
-	}
-
-/*Create Exponent*/
-	for (i = 1; i < len; i++)
-		expo *= 10;
-/*Main */
-	while (expo > 1)
-	{
-		_putchar((res / expo) % 10 + '0');
-		expo /= 10;
-	}
-	_putchar(res % 10 + '0');
+if ((n < 0) && (n >= -9))
+{
+_putchar('-');
+_putchar((n * -1) + '0');
 }
+else if (n <= -10)
+{
+_putchar('-');
+_putchar(((n / 10) * -1) + '0');
+_putchar(((n % 10)  * -1) + '0');
+}
+else if (n == 0)
+{
+_putchar(n + '0');
+}
+else if ((n > 0) && (n <= 9))
+{
+_putchar(n + '0');
+}
+else if ((n >= 10) && (n <= 99))
+{
+_putchar((n / 10) + '0');
+_putchar((n % 10) + '0');
+}
+else if ((n >= 100) && (n <= 999))
+{
+_putchar((n / 100) + '0');
+_putchar(((n / 10) % 10) + '0');
+_putchar((n % 10) + '0');
+}
+else if ((n >= 1000) && (n <= 9999))
+{
+_putchar((n / 1000) + '0');
+_putchar((n / 100) % 10 + '0');
+_putchar((n / 10) % 10 + '0');
+_putchar((n % 10) + '0');
+}
+}
+
