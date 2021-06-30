713b4f361da784615ba4ed4fd24f1efa10a56591
justmvrco <moreirapiliki@gmail.com>
2021-06-30 10:03:51 -0700

    marco

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverse an array
+ * @a: first input
+ * @n: number of elemets
+ *
+ * Description: reverses the content of an array of integers
+ * Return: always (0)
+ */
+
+void reverse_array(int *a, int n)
+{
+int left, right,  tmp;
+left = 0;
+right = n - 1;
+while (left < right)
+{
+tmp = a[left];
+a[left] = a[right];
+a[right] = tmp;
+left++;
+right--;
+}
+}
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * leet - encode string
+ * @str: input string
+ *
+ * Description: encodes a string into 1337
+ * Return: always (0
+ */
+
+char *leet(char *str)
+{
+int i = 0, k;
+char s[] = "aAeEoOtTlL";
+char s1[] = "4433007711";
+for (; str[i] != '\0'; i++)
+{
+for (k = 0; k <= 9; k++)
+{
+if (s[k] == str[i])
+{
+str[i] = s1[k];
+}
+}
+}
+return (str);
+}
