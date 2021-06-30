5734acfab3b574f6673c5fd2e9f2d91975d62819
justmvrco <moreirapiliki@gmail.com>
2021-06-30 10:28:03 -0700

    marco

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * rot13 - encode string
+ * @str: input string
+ *
+ * Description: encodes a string using rot13
+ * Return: always (0)
+ */
+
+char *rot13(char *str)
+{
+int i = 0, j;
+char s[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char s1[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (; str[i] != '\0'; i++)
+{
+for (j = 0; j <= 51; j++)
+{
+if (s[j] == str[i])
+{
+str[i] = s1[j];
+}
+}
+}
+return (str);
+}
