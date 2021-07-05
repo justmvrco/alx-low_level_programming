15436c91f3315f0b78345ee3eea49fe8114471e2
justmvrco <moreirapiliki@gmail.com>
2021-07-05 12:59:48 -0700

    marco

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - print chessboard
+ * @a: input chars
+ *
+ * Description: print_chessboard
+ * Returns: always 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+_putchar(a[i][j]);
+}
+_putchar('\n');
+}
+}
