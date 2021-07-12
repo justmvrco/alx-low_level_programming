7cd4a0006e047699469d520df7af1d6e699684bf
justmvrco <moreirapiliki@gmail.com>
2021-07-12 10:55:33 -0700

    marco

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,20 @@
-#include "holberton.h"
-
-int main(void)
-{
-	_puts("\"my goal was to be the best\"\n\t- marco");
-	return (0);
-}
+(^0-isupper.o
+0-memset.o
+0-strcat.o
+1-isdigit.o
+1-memcpy.o
+1-strncat.o
+100-atoi.o
+2-strchr.o
+2-strlen.o
+2-strncpy.o
+3-islower.o
+3-puts.o
+3-strcmp.o
+3-strspn.o
+4-isalpha.o
+4-strpbrk.o
+5-strstr.o
+6-abs.o
+9-strcpy.o
+ )_putchar.o
