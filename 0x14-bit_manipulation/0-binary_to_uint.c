f0df7dd79512b986fbc9bd1aa803634182277d9f
justmvrco <moreirapiliki@gmail.com>
2021-08-05 09:02:19 -0700

    marco

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,73 +1,29 @@
 #include "holberton.h"
-/**
-  *_strlen - returns the length of a string.
-  *@s: pointer to string.
-  *
-  *Return: length.
-  */
-unsigned int _strlen(const char *s)
-{
-	unsigned int i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
 
 /**
-  *getp - returns the value of x to the power of y.
-  *@x: number.
-  *@y: power.
-  *
-  *Return: x to the pow of y.
-  *0 if x < 0.
-  */
-int getp(int x, int y)
-{
-	if (y < 0)
-		return (0);
-	if (y == 0)
-		return (1);
-	if (y == 1)
-		return (x);
-
-	return (x * getp(x, y - 1));
-}
-
-/**
-  *binary_to_uint - converts a binary number to an unsigned int.
-  *@b: pointer to string containing 0 and 1.
-  *
-  *Return: converted number or 0 if b is null or has chars not 0 or 1.
-  */
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: pointer to a string containing a binary number
+ *
+ * Return: unsigned int with decimal value of binsry number, or 0 if error
+ */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num, length, index;
-	int power;
+	int i;
+	unsigned int num;
 
+	num = 0;
 	if (!b)
 		return (0);
-
-	length = _strlen(b);
-	power = 0;
-	num = 0;
-	index = length - 1;
-	while (length > 0)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[index] == 48 || b[index] == 49)
-		{
-			num = num + ((b[index] - 48) * getp(2, power));
-			power++;
-			index--;
-			length--;
-		}
-		else
-		{
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		}
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		num <<= 1;
+		if (b[i] == '1')
+			num += 1;
 	}
 	return (num);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -22,3 +22,4 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 
 #endif
+
